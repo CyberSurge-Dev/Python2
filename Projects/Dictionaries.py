5f244a6eb1802876152b68f13b726adc9adeeafc
CyberSurge <82483553+CyberSurge-Dev@users.noreply.github.com>
2023-08-29 12:54:56 -0400

    Add files via upload

--- Projects/Dictionaries.py
+++ Projects/Dictionaries.py
@@ -8,30 +8,36 @@ def print_header(title):
 
 print_header("Task 1 - Alphabetical Order")
 
-# Get users input 
-usr_in = input("  Enter a sentance: ")
+while True:
+    # Get users input 
+    usr_in = input("  Enter a sentence: ")
 
-# Create dictionary for letter counts
-counts = {}
+    # Create dictionary for letter counts
+    counts = {}
 
-# Loop through sentance for characters
-for letter in list(usr_in):
-    if letter.isalpha():
-        if counts.get(letter, False) != False:
-            counts[letter.lower()] += 1
-        else:
-            counts[letter.lower()] = 1
+    # Loop through sentance for characters
+    for letter in list(usr_in):
+        if letter.isalpha():
+            if counts.get(letter, False) != False:
+                counts[letter.lower()] += 1
+            else:
+                counts[letter.lower()] = 1
 
-# Cycle through sorted keys and print in alphabetical order
-print("\n  Letter count:")
-for key in sorted(list(counts.keys())):
-    print(f"    {key} - {counts[key]}")
+    # Cycle through sorted keys and print in alphabetical order
+    print("\n  Letter count:")
+    for key in sorted(list(counts.keys())):
+        print(f"    {key} - {counts[key]}")
+        
+    # Check if user wants to submit another response
+    usr_in = input("\n  Enter another sentence? (y/n): ")
+    if usr_in.lower() == 'n' or usr_in.lower() == 'no':
+        break
 
 input("\n Press Enter to Continue...")
 print_header("Task 1 - Alphabetical Order")
 
 
-def translator(sentance):
+def translator(sentence):
     """This function translates any string passed in, in to pirate speak and returns the result."""
     
     # Dictionary for translation
@@ -56,30 +62,36 @@ def translator(sentance):
         'man':'matey'
     }
     # Make all characters lowercase
-    sentance = sentance.lower()
+    sentence = sentence.lower()
     # Index placeholder variable
     index = 0
     # Split words from sentance into a list using split()
-    sentance = sentance.split(' ')
+    sentence = sentence.split(' ')
     # Cycle through words in the list using for loop
-    for word in sentance:
+    for word in sentence:
         # check if there is a translation
         if word in list(pirate_translator.keys()):
             # Replace the word in the list
-            sentance[index] = pirate_translator[word]
+            sentence[index] = pirate_translator[word]
         index += 1
         
     # Repack sentance into a string
     out_str = ""
-    for word in sentance:
+    for word in sentence:
         out_str += (word+" ")
         
     # Return new sentance
     return out_str
 
-# Get user input
-usr_in = input("  Enter a sentance (no punctuation): ")
-# Print out the result from the translator function
-print(f"\n  Translated:\n  {translator(usr_in)}")
-
+while True:
+    # Get user input
+    usr_in = input("  Enter a sentence (no punctuation): ")
+    # Print out the result from the translator function
+    print(f"\n  Translated:\n  {translator(usr_in)}")
+    
+    # Check if user wants to submit another response
+    usr_in = input("\n  Enter another sentence? (y/n): ")
+    if usr_in.lower() == 'n' or usr_in.lower() == 'no':
+        break
+    
 input("\n Press Enter to Continue...")
